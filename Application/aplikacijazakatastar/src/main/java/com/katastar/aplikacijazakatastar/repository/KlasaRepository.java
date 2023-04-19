5e1984dc7bca3b0e8ac7aecca8ce78b030e2e881
Oliver1234-eng <oliver.kojic22@gmail.com>
2023-04-19 19:09:20 +0200

    Uradjene promene sa konekcijama na mysql bazu

---
+++ Application/aplikacijazakatastar/src/main/java/com/katastar/aplikacijazakatastar/repository/KlasaRepository.java
@@ -0,0 +1,7 @@
+package com.katastar.aplikacijazakatastar.repository;
+
+import com.katastar.aplikacijazakatastar.model.Klasa;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KlasaRepository extends JpaRepository<String, Integer> {
+}
